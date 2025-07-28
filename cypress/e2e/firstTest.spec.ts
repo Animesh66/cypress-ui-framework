dc73e804850bd93cff4e60aad76969824d14fd81
Animesh66 <er.animesh6@hotmail.com>
2025-07-28 20:27:22 +1000

    Fixed the issue.

--- cypress/e2e/firstTest.spec.ts
+++ cypress/e2e/firstTest.spec.ts
@@ -5,8 +5,10 @@ describe('This is my first test', () => {
         // Navigate to base url
         cy.visit('/');
         // Go to search product searchbox and type product name
-        cy.get('.search-box-text ui-autocomplete-input').type('Laptop');
+        cy.get('[class="search-box-text ui-autocomplete-input"]').type('Laptop');
         // Click on search button
-        cy.get('.button-1 search-box-button').click();
+        cy.get('[value="Search"]').click();
+        // Verify that the search results page is displayed
+        cy.get('h1').should('have.text', 'Search');
     })
 })
