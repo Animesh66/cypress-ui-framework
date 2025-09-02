4ab8bb1774cbb1f698037e1d71180fe2a7bcefeb
Animesh66 <er.animesh6@hotmail.com>
2025-09-02 20:30:16 +1000

    Added login test.

--- cypress/e2e/demo-webshop/loginTest.spec.ts
+++ cypress/e2e/demo-webshop/loginTest.spec.ts
@@ -3,9 +3,12 @@
 import { LoginPage } from "../../support/pages/loginPage";
 import TopNavigation from "../../support/components/topNavigation";
 import HeaderMenu from "../../support/components/headerMenu";
+import MyAccountPage from "../../support/pages/myAccountPage";
+import { data } from "cypress/types/jquery";
 
 describe('Test conatining user login flow', () => {
     const loginPage = new LoginPage();
+    const myAccount = new MyAccountPage();
     beforeEach(() => {
         loginPage.navigateToPage();
     })
@@ -13,6 +16,7 @@ describe('Test conatining user login flow', () => {
         TopNavigation.clickOnLoginNavigation();
         cy.fixture('login.json').then((data) => {
             loginPage.login(data.email, data.password);
+            myAccount.verifyMyAccountPage(data.email);
         });
         HeaderMenu.clickOnAccessories();
     })
