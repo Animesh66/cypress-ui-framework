db7b0362b25cea082e27d76d53f95fe5d936d5c3
Animesh66 <er.animesh6@hotmail.com>
2025-09-02 21:01:27 +1000

    Added log to test.

--- cypress/e2e/demo-webshop/loginTest.spec.ts
+++ cypress/e2e/demo-webshop/loginTest.spec.ts
@@ -5,6 +5,7 @@ import TopNavigation from "../../support/components/topNavigation";
 import HeaderMenu from "../../support/components/headerMenu";
 import MyAccountPage from "../../support/pages/myAccountPage";
 import { data } from "cypress/types/jquery";
+import { Logger } from "../../support/utils/logger";
 
 describe('Test conatining user login flow', () => {
     const loginPage = new LoginPage();
@@ -13,11 +14,17 @@ describe('Test conatining user login flow', () => {
         loginPage.navigateToPage();
     })
     it.only('User login flow happy path scenario works as expected', () => {
+        Logger.info('Navigating to login page');
         TopNavigation.clickOnLoginNavigation();
+        Logger.info('Succesfully navigated to login page');
         cy.fixture('login.json').then((data) => {
+            Logger.info('Logging in with valid credentials');
             loginPage.login(data.email, data.password);
+            Logger.info('Succesfully logged in with valid credentials');
             myAccount.verifyMyAccountPage(data.email);
         });
+        Logger.info('Clicking on accessories');
         HeaderMenu.clickOnAccessories();
+        Logger.info('Succesfully clicked on accessories');
     })
 })
--- cypress/e2e/demo-webshop/registrationTest.spec.ts
+++ cypress/e2e/demo-webshop/registrationTest.spec.ts
@@ -4,18 +4,24 @@ import Data from "../../../testData/data";
 import RegistrationPage from "../../support/pages/registrationPage";
 import TopNavigation from "../../support/components/topNavigation";
 import MyAccountPage from "../../support/pages/myAccountPage";
+import { Logger } from "../../support/utils/logger";
 
 describe('Test conatining user registration flow', () => {
     const regPage = new RegistrationPage();
     const myAccount = new MyAccountPage();
 
     beforeEach(() => {
-        regPage.navigateToPage();
+        Logger.info('Navigating to registration page');
+        TopNavigation.clickOnRegisterNavigation();
+        Logger.info('Succesfully navigated to registration page');
     })
     it('User registration flow happy path scenario works as expected', () => {
-        TopNavigation.clickOnRegisterNavigation();
         const email = Data.Email;
+        Logger.info('Registering user with email ' + email);
         regPage.registerUser(Data.Gender("female"), Data.FirstName, Data.LastName, email, Data.Password);
+        Logger.info('Succesfully registered user with email ' + email);
+        Logger.info('Verifying my account page');
         myAccount.verifyMyAccountPage(email);
+        Logger.info('Succesfully verified my account page');
     })
 })
