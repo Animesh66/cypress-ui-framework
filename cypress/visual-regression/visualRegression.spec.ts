1d2cb4c5b41922d38cf7d79d761612005e1e86f5
Animesh66 <er.animesh6@hotmail.com>
2025-09-11 22:44:15 +1000

    Updated the yaml file.

--- cypress/visual-regression/visualRegression.spec.ts
+++ cypress/visual-regression/visualRegression.spec.ts
@@ -53,70 +53,31 @@ type ViewportSpec = {
       context(`Viewport: ${vp.name}`, () => {
         beforeEach(() => {
           setViewport(vp)
-          cy.visit('/', { timeout: 60000 })
+          cy.visit('/')
           stabilizeUI()
         })
   
         it(`Homepage - full page and key elements (${vp.name})`, () => {
-          cy.visit(pages.home, { timeout: 60000 })
-  
-          cy.get('div.header', { timeout: 20000 }).should('be.visible')
-          cy.get('.header-logo img', { timeout: 20000 }).should('be.visible')
-          cy.get('form#small-search-box-form', { timeout: 20000 }).should('be.visible')
-          cy.get('ul.top-menu', { timeout: 20000 }).should('exist')
-  
-          // Full page
-          cy.compareSnapshot(`home-full-${vp.name}`, 0.1)
-  
-          // Key elements
-          cy.get('div.header').compareSnapshot(`home-header-${vp.name}`, 0.1)
-          cy.get('.header-logo img').compareSnapshot(`home-logo-${vp.name}`, 0.1)
-          cy.get('form#small-search-box-form').compareSnapshot(`home-search-${vp.name}`, 0.1)
-          cy.get('div.footer').compareSnapshot(`home-footer-${vp.name}`, 0.1)
+          cy.visit(pages.home);
+          cy.get('.header').should('be.visible');
+          cy.compareSnapshot(`home-full-${vp.name}`, 0.1);
+          cy.get('.header').compareSnapshot(`home-header-${vp.name}`, 0.1);
         })
   
         it(`Login - full page and key elements (${vp.name})`, () => {
-          cy.visit(pages.login, { timeout: 60000 })
-  
-          cy.get('h1', { timeout: 20000 }).should('contain.text', 'Login')
-          cy.get('form#returningCustomerForm, form#login-form', { timeout: 20000 }).should('exist')
-  
-          // Full page
-          cy.compareSnapshot(`login-full-${vp.name}`, 0.1)
-  
-          // Key elements
-          cy.get('form#returningCustomerForm, form#login-form')
-            .first()
-            .compareSnapshot(`login-form-${vp.name}`, 0.1)
-  
-          cy.get('input#Email').should('exist').compareSnapshot(`login-email-${vp.name}`, 0.1)
-          cy.get('input#Password').should('exist').compareSnapshot(`login-password-${vp.name}`, 0.1)
-          cy.get('input.button-1.login-button, button.login-button')
-            .first()
-            .should('exist')
-            .compareSnapshot(`login-button-${vp.name}`, 0.1)
+          cy.visit(pages.login)
+          cy.get('.header').should('be.visible');
+          cy.get('h1').should('contain.text', 'Login');
+          cy.compareSnapshot(`login-full-${vp.name}`, 0.1);
+          cy.get('.header').compareSnapshot(`home-header-${vp.name}`, 0.1);
         })
   
         it(`Registration - full page and key elements (${vp.name})`, () => {
-          cy.visit(pages.register, { timeout: 60000 })
-  
-          cy.get('h1', { timeout: 20000 }).should('contain.text', 'Register')
-          cy.get('form#register-form', { timeout: 20000 }).should('exist')
-  
-          // Full page
-          cy.compareSnapshot(`register-full-${vp.name}`, 0.1)
-  
-          // Key elements
-          cy.get('form#register-form').compareSnapshot(`register-form-${vp.name}`, 0.1)
-          cy.get('input#FirstName').should('exist').compareSnapshot(`register-firstname-${vp.name}`, 0.1)
-          cy.get('input#LastName').should('exist').compareSnapshot(`register-lastname-${vp.name}`, 0.1)
-          cy.get('input#Email').should('exist').compareSnapshot(`register-email-${vp.name}`, 0.1)
-          cy.get('input#Password').should('exist').compareSnapshot(`register-password-${vp.name}`, 0.1)
-          cy.get('input#ConfirmPassword').should('exist').compareSnapshot(`register-confirm-${vp.name}`, 0.1)
-          cy.get('input#register-button, button#register-button')
-            .first()
-            .should('exist')
-            .compareSnapshot(`register-button-${vp.name}`, 0.1)
+          cy.visit(pages.register)
+          cy.get('.header').should('be.visible');
+          cy.get('h1').should('contain.text', 'Register');
+          cy.compareSnapshot(`register-full-${vp.name}`, 0.1);
+          cy.get('.header').compareSnapshot(`home-header-${vp.name}`, 0.1);
         })
       })
     })
